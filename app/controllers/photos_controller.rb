64a887a4f3bcd72ddc9365a65d6353eb557bb305
cflicher <cflicher@users.noreply.github.com>
2016-10-26 14:13:38 -0500

    final updates

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -23,11 +23,23 @@ class PhotosController < ApplicationController
     redirect_to("http://localhost:3000/photos")
   end
 
-  def delete_row
+  def destroy
     p = Photo.find_by({:id => params[:id]})
     p.destroy
 
     redirect_to("http://localhost:3000/photos")
   end
 
+  def edit_form
+    @photo = Photo.find_by({:id => params[:id]})
+  end
+
+  def update_row
+    p = Photo.find_by({:id => params[:id]})
+    p.caption = params[:the_caption]
+    p.source  = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get("/create_photo",     { :controller => "photos", :action => "create_row" })
 
   #for Deletion
-  get("/delete_photo/:id", { :controller => "photos", :action => "delete_row" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+# for editing photos
+  get("/photos/:id/edit",  { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
